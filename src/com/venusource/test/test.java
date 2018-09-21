aaf41f8f1239867196334d9329526dcf1e0a9108
jerry9926 <jerry9926@163.com>
2018-09-21 10:23:03 +0800

    add a line

--- src/com/venusource/test/test.java
+++ src/com/venusource/test/test.java
@@ -2,5 +2,6 @@ class test{
    public static void main(String[] args){
      System.out.println("hello, world!");
      System.out.println("my first open source contribution");
+     System.out.println("add a line")
    }
 }
