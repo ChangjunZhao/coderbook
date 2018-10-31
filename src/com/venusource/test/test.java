e7136525cf93b8d6b79be5f75bd47e2097c091ac
gitcoffee2017 <33301690+gitcoffee2017@users.noreply.github.com>
2018-10-31 13:06:31 +0800

    Added a new line
    for test.

--- src/com/venusource/test/test.java
+++ src/com/venusource/test/test.java
@@ -3,6 +3,6 @@ class test{
      System.out.println("hello, world!");
      System.out.println("my first open source contribution");
      System.out.println("add a line")
-     System.out.println("added a new line for test")
+     System.out.println("Added a new line for test")
    }
 }
