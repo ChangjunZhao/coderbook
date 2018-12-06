c65f0bc11a67387ca8ce00c8c2234aecf289d8e5
Dreamer_he <sysuheyuan@gmail.com>
2018-12-05 23:17:28 -0600

    add the semicolon to the last system out statement

--- src/com/venusource/test/test.java
+++ src/com/venusource/test/test.java
@@ -2,6 +2,6 @@ class test{
    public static void main(String[] args){
      System.out.println("hello, world!");
      System.out.println("my first open source contribution");
-     System.out.println("add a line")
+     System.out.println("add a line");
    }
 }
