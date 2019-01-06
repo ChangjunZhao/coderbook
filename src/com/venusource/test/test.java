b016a1ef8ef9658785fb4544392d00dd44ddea1f
wentian <961913813@qq.com>
2019-01-06 18:37:43 +0800

    add a new test !!!

--- src/com/venusource/test/test.java
+++ src/com/venusource/test/test.java
@@ -2,7 +2,8 @@ class test{
    public static void main(String[] args){
      System.out.println("hello, world!");
      System.out.println("my first open source contribution");
-     System.out.println("add a line")
-     System.out.println("Added a new line for test")
+     System.out.println("add a line");
+     System.out.println("Added a new line for test");
+     System.out.println("add a new test !!!!");
    }
 }
